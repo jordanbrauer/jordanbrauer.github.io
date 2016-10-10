461687214fdae93a566044ededa557eac77cc20f
jordanbrauer <jbrauer.inc@gmail.com>
2016-10-10 10:26:53 -0500

    Created a simple boilerplate for my gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+// gulpfile.js
+
+/** Dependencies ================================
+ */
+var gulp = require('gulp');
+
+/** Variables ===================================
+ */
+
+/** Gulp Tasks ==================================
+ */
+
+// default task
+gulp.task('default', function() {
+  // ... ... ...
+});
